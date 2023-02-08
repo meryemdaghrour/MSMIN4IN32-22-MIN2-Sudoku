7d0ed633aecdc6727889c39e4585e7f18c1c28d1
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-02-08 17:06:18 +0100

    version fonctionnelle

--- Sudoku.CNN/CNNSolver.cs
+++ Sudoku.CNN/CNNSolver.cs
@@ -1,7 +1,12 @@
-﻿namespace Sudoku.CNN
-{
-    public class EmptySolver
-    {
+﻿using Sudoku.Shared;
 
+namespace Sudoku.CNN
+{
+    public class CNNSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
     }
 }
