e9836de50ac96861705f049e80190249d8b97db4
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-02-15 12:11:15 +0100

    Première version CNNsolver qui résout à partir de la méthode de résolution Z3DEMO

--- Sudoku.CNN/CNNSolver.cs
+++ Sudoku.CNN/CNNSolver.cs
@@ -1,12 +1,51 @@
-﻿using Sudoku.Shared;
+﻿using Python.Runtime;
+using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Sudoku.CNN
 {
-    public class CNNSolver : ISudokuSolver
+    internal class CNNPythonSolver : PythonSolverBase
     {
-        public SudokuGrid Solve(SudokuGrid s)
+
+
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
         {
-            return s.CloneSudoku();
+
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cells.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("instance", pyCells);
+
+                // the person object may now be used in Python
+                string code = Resource1.Z3Solver_py;
+                scope.Exec(code);
+                var result = scope.Get("r");
+                var managedResult = result.As<int[][]>();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                return new Shared.SudokuGrid() { Cells = managedResult };
+            }
+            //}
+
         }
+
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("z3-solver");
+            base.InitializePythonComponents();
+        }
+
+
+
     }
 }
