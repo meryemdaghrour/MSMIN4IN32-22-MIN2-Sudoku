e3d99bc14a5ca9cd2b0439da1c54eaeae1cae46e
AurelieVidal <aurelie.vidal@epfedu.fr>
2023-02-20 10:19:08 +0100

    code c++ adapté

--- Norvig2/SudokuNorvig2.cs
+++ Norvig2/SudokuNorvig2.cs
@@ -6,9 +6,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Sudoku.Shared;
+using System.Runtime.ConstrainedExecution;
+using Iced.Intel;
+using Dia2Lib;
+using System.Numerics;
+using System.Globalization;
+
+public class SudokuNorvig2 : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+
+        int[][] tab = s.Cells;
+
+        String line = "";
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+
+                line += tab[i][j];
+            }
+
+        }
+
+        Sudoku2.Init();
+
+        
+
+        
+        Sudoku2 S = new Sudoku2(line); //trouver cmt inclure line dans Sudoku2 : adapter constructeur
+        //tout marche bien ici ! 
+        //->application des contraintes bonne, le probleme est pour les resolutions complexes
+        
+        
+
+        SolveResult sol = SolveResult.Solve(S); //probleme
+        S = sol.Result;
+
+        SudokuGrid sudoku = new SudokuGrid();
+        int[][] tab2 = new int[9][];
+        for (int i = 0; i < tab2.Length; i++)
+        {
+            tab2[i] = new int[9];
+        }
+
+        if (S.IsSolved())
+        {
+            int cpt = 0;
+            int c = 0;
+            for (int l = 0; l < 81; l++)
+            {
+                //S._cells[l];//liste
+                int result = 0;
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        
+                        if (S._cells[l]._b[j])
+                        {
+                            result = j+1;
+                        }
+                    }
+
+                    
+                    tab2[c][cpt] = result;
+
+
+
+
+                    cpt++;
+
+                    if (cpt == 9)
+                    {
+                        cpt = 0;
+                        c++;
+                    }
+
+                }
+                //remplir une ligne dans un tableau 2 dim
+            }
+        }
+        else
+        {
+            Console.WriteLine("No solution");//return s.CloneSudoku();
+        }
+            
+
+        sudoku.Cells = tab2;
+
+        //a remplir en utilisant la méthode solve dans solveresult
+        return sudoku;
+        // de Sudoku 2 à SudokuGrid : mettre des true sur le n° _cell correspondant
+    }
+
+}
+
+
+
 class Possible
 {
-    private bool[] _b;
+    public bool[] _b;
 
     public Possible()
     {
@@ -19,6 +118,7 @@ class Possible
     {
         return _b[i - 1];
     }
+    
 
     public int count()
     {
@@ -65,7 +165,12 @@ class Possible
 }
 class Sudoku2
 {
-    private List<Possible> _cells=new List<Possible>();
+    public List<Possible> _cells = new List<Possible>();
+
+    /*
+            {
+                new Possible(),
+            }*/
     private static List<List<int>> _group;
     private static List<List<int>> _neighbors;
     private static List<List<int>> _groups_of;
@@ -73,7 +178,10 @@ class Sudoku2
 
 
     private bool Eliminate(int k, int val)
+
     {
+        //Console.WriteLine(k);
+        //Console.WriteLine("test pour case : " + k+" valeur : "+val);
         Possible cell = _cells[k];
         if (!cell.is_on(val))
         {
@@ -85,6 +193,8 @@ class Sudoku2
 
         if (N == 0)
         {
+            
+            //Console.WriteLine("test pour case : " + k + " valeur : " + val);
             return false;
         }
         else if (N == 1)
@@ -122,44 +232,198 @@ class Sudoku2
         return true;
     }
 
-    public Sudoku2(string s)
+    static public Boolean pb = false;
+
+    public Sudoku2(String s) 
     {
-        // Implementation not provided
+        pb = false;
+        
+        _cells = new List<Possible>
+        {
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),new Possible(),
+        new Possible()
+        };
+        //modifier _cells !!!!
+        int k = 0;
+        //int test = 0;
+        //Console.WriteLine(s[78]);
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            /*
+            if (test == 1)
+            {
+                //Console.WriteLine("ERREUR ");
+                pb = true;
+            }
+            test = 1;*/
+            //Console.WriteLine(s[i]);
+            //Console.WriteLine(k);
+            //OK
+            if (s[i] >= '1' && s[i] <= '9')
+            {
+                //ok
+
+                if (!Assign(k, s[i] - '0'))
+                {
+                    //Console.WriteLine("i = " + i);
+                    //Console.WriteLine("k = " + k);
+                    //Console.WriteLine(s[i] - '0');
+                    pb = true;
+                }
+                k++;
+            }
+            else if (s[i] == '0' || s[i] == '.')
+            {
+                k++;
+                //ok
+            }
+            //test = 2;
+           // Console.WriteLine(i);
+            //Console.WriteLine("k = "+k);
+
+
+        }
+
+        
+
+
+    }
+
+    public Sudoku2()
+    {
+    }
+
+    public Sudoku2(Sudoku2 S)
+    {
+        this._cells = S._cells;
+    }
+
+    public String Line (int k, int i)
+    {
+        String line = "";
+
+        //modification de la case
+
+        for (int l = 0; l < 9; l++)
+        {
+            this._cells[k]._b[l] = false;
+        }
+        this._cells[k]._b[i-1] = true;
+        
+
+        //Conversion en String
+        for (int l = 0; l < 81; l++)
+        {
+            int m = _cells[l].count();
+
+            if (m == 1)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (_cells[l]._b[j] == true)
+                    {
+                        line += j + 1;
+                    }
+                }
+            }
+            else
+            {
+                line += "0";
+            }
+
+        }
+        
+        return line;
+
+    }
+
+
+    public Boolean Success()
+    {
+        for (int i=0; i < _cells.Count; i++)
+        {
+            int m = _cells[i].count();  
+            if (m != 1)
+            {
+                return false;
+            }
+        }
+
+        return true; 
     }
 
     public static void Init()
     {
-        static void Init()
+
+        _group = new List<List<int>>
+            {
+                new List<int> {}, new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{}
+             };
+        _neighbors = new List<List<int>>
+            {
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{}
+            };
+        _groups_of = new List<List<int>>
+            {
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},new List<int>{},
+                new List<int>{}
+            };
+        for (int i = 0; i < 9; i++)
         {
-            _group = new List<List<int>>(27);
-            _neighbors = new List<List<int>>(81);
-            _groups_of = new List<List<int>>(81);
-            for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
             {
-                for (int j = 0; j < 9; j++)
+                int k = i * 9 + j;
+                int[] x = new int[3] { i, 9 + j, 18 + (i / 3) * 3 + j / 3 };
+                for (int g = 0; g < 3; g++)
                 {
-                    int k = i * 9 + j;
-                    int[] x = new int[3] { i, 9 + j, 18 + (i / 3) * 3 + j / 3 };
-                    for (int g = 0; g < 3; g++)
-                    {
-                        _group[x[g]].Add(k);
-                        _groups_of[k].Add(x[g]);
-                    }
-                }
-            }
-            for (int k = 0; k < _neighbors.Count; k++)
-            {
-                for (int x = 0; x < _groups_of[k].Count; x++)
-                {
-                    for (int j = 0; j < 9; j++)
-                    {
-                        int k2 = _group[_groups_of[k][x]][j];
-                        if (k2 != k) _neighbors[k].Add(k2);
-                    }
+
+                    _group[x[g]].Add(k);
+                    _groups_of[k].Add(x[g]);
+
                 }
             }
         }
+        for (int k = 0; k < _neighbors.Count; k++)
+        {
+            for (int x = 0; x < _groups_of[k].Count; x++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int k2 = _group[_groups_of[k][x]][j];
+                    if (k2 != k) _neighbors[k].Add(k2);
+                }
+            }
+        }
+        
     }
+
+    
 
     public Possible Possible(int k)
     {
@@ -180,6 +444,7 @@ class Sudoku2
 
     public bool Assign(int k, int val)
     {
+        
         for (int i = 1; i <= 9; i++)
         {
             if (i != val)
@@ -196,15 +461,22 @@ class Sudoku2
         int min = int.MaxValue;
         for (int i = 0; i < _cells.Count; i++)
         {
+            //Console.WriteLine(i);
+            //Console.WriteLine(k);
             int m = _cells[i].count();
+            //Console.WriteLine(m);
+
+
             if (m > 1 && (k == -1 || m < min))
             {
                 min = m;
                 k = i;
             }
+            //Console.WriteLine();
         }
+        //Console.WriteLine("k = "+k);
         return k;
-    }
+    } //
 
     public void Write(TextWriter o)
     {
@@ -243,55 +515,90 @@ class myClass : ICloneable
 }
 class SolveResult
 {
+
     public Sudoku2 Result { get; set; }
     public bool IsSuccess { get; set; }
 
+
     public static SolveResult Solve(Sudoku2 S)
     {
+        
+
+        //cells OK
+        //grille de sudoku sudoku2
+        //solveresult renvoie un booleen qui dit si la resolution est un sicces o non et une grille de sudoku résolue.
         if (S == null || S.IsSolved())
         {
             return new SolveResult() { Result = S, IsSuccess = true };
         }
 
-        int k = S.LeastCount();
+        //si pas de grille ou grille résolue, on renvoie la grille initiale
+        int k = S.LeastCount(); //k=8 : premiere case à seulement 2 possibilités
+        //permettrait de connaitre la premiere case qui a plusieurs true
+        //Console.WriteLine("k = " + k);
         Possible p = S.Possible(k);
+        List<Boolean> tab = new List<Boolean>() { false,false,false,false,false,false,false,false,false  };
+
         for (int i = 1; i <= 9; i++)
         {
-            if (p.is_on(i))
+            tab[i-1] = p.is_on(i);
+
+        }
+
+
+            //for (int i = 1; i <= 9; i++)
+            for (int i = 1; i <= 9; i++)
+        {
+
+            //repérer la premiere possibilité
+            //Console.WriteLine(p.is_on(i-1));
+
+            //if (p.is_on(i)) //si le booléen est true
+                             //if (p.is_on(i))
+            if (tab[i-1]==true)
             {
-                Sudoku2 S1 = new Sudoku2(S);
-                if (S1.Assign(k, i))
+
+                
+                Boolean ok = false;
+
+                //faire un clone avec une seule difference de S
+
+                String line = S.Line(k, i);
+                Sudoku2.Init();
+                Sudoku2 S1 = new Sudoku2(line);
+                //clone S
+                
+                //Console.WriteLine("cells de S1");
+                
+                ok = Sudoku2.pb;
+                Boolean cont = false;
+                if (!ok)
+                {
+                    cont = S1.Assign(k, i);
+                }
+
+
+
+                
+                //if (S1.Assign(k, i))
+                if (cont)
+
                 {
                     SolveResult result = Solve(S1);
+
+
                     if (result.IsSuccess)
                     {
                         return result;
                     }
                 }
             }
+            //Console.WriteLine();
         }
+        //je suis ici
         return new SolveResult() { Result = null, IsSuccess = false };
     }
 
-}
-class Program2
-{
-    static void Main(string[] args)
-    {
-        Sudoku2.Init();
-        string line;
-        while ((line = Console.ReadLine()) != null)
-        {
-            Sudoku2 S = new Sudoku2(line);
-            if (S.IsSolved())
-            {
-                S.Write(Console.Out);
-            }
-            else
-            {
-                Console.WriteLine("No solution");
-            }
-            Console.WriteLine();
-        }
-    }
+
+
 }
