dfe66c0da34999ac22c549873cd2fe4505cb4278
minigandii <clement.gandillon@gmail.com>
2023-02-21 19:52:53 +0100

    Ajout 2eme solver vide

---
+++ Sudoku.GraphColoring2/SolverGraphColoring2.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.GraphColoring2
+{
+    public class SolverGraphColoring2 : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
