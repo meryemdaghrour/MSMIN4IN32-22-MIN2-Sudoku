4e2e4266fb030ae10aacfe0d3f9f91e83d1a0692
GuillaumeBdrd <guillaume.bodard@orange.fr>
2023-02-21 16:07:32 +0100

    Ajout solver 1 Graph Coloration

--- Sodoku.GraphColoring/SolverGraphColoring.cs
+++ Sodoku.GraphColoring/SolverGraphColoring.cs
@@ -4,15 +4,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SudokuSolver;
+using System.Diagnostics;
 
 
-namespace Sudoku.GraphColoring 
-{ 
-    public class SolverGraphColoring : ISudokuSolver
+namespace Sodoku.GraphColoring
+{
+    public class ColoratSolionSolver : ISudokuSolver
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
+            var puzzle = new SudokuPuzzle();
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (s.Cells[i - 1][j - 1] == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        puzzle[i, j] = s.Cells[i - 1][j - 1];
+                    }
+                }
+            }
+            Console.WriteLine(puzzle);
+            var stopWatch = Stopwatch.StartNew();
+            var result = puzzle.Solve();
+            stopWatch.Stop();
+
+            if (result)
+                Console.WriteLine(puzzle);
+            else { 
+                Console.WriteLine("Puzzle couldn't be solved");
+                return s.CloneSudoku();
+            }
+
+            Console.WriteLine("Elapsed Time: {0}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    s.Cells[i - 1][j - 1] = (int)puzzle[i, j];
+                }
+            }
+
             return s.CloneSudoku();
         }
     }
+
 }
